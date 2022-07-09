ddffbf309789d3ca048b3221a11f5cddad5f1298
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-07-09 14:33:27 -0300

    agregando sweet alert.js

--- program.js
+++ program.js
@@ -9,40 +9,38 @@ let captura_boton = function () {
 };
 
 let dataRead = function () {
- 
   let myObject = {
     nombre: document.querySelector("#nombre").value,
     apellido: document.querySelector("#apellido").value,
     email: document.querySelector("#email").value,
     pass: document.querySelector("#pass").value,
   };
-  
+
   //GUARDAMOS EL LOS DATOS DEL LOGIN TRANSFORMADOS EN JSON EN EL LOCALESTORAGE
 
   let storage = JSON.stringify(myObject);
   localStorage.setItem("miInfo", storage);
-  console.log(myObject)
-
+  console.log(myObject);
 };
 
-
 document.querySelector("#registro").addEventListener("click", () => {
-
   if (
-
     document.getElementById("nombre").value.length == 0 ||
-
     document.getElementById("apellido").value.length == 0
-
   ) {
 
-    document.getElementById("mensaje").innerHTML = "Debe completar los campos requeridos";
-
+    // LIBRERIA
+    let btn = document.getElementById("registro");
+    btn.addEventListener("click", () => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    });
+     // LIBRERIA FIN
   } else {
-
     window.location.href = "inicio.html";
-
   }
-
 });
 main();
